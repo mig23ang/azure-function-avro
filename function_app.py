ab977a6fd1f9fb7aeaa59092c2ac9eff9fac44a3
miguel angel reyes <miguel.reyesc@usantoto.edu.co>
2024-11-05 13:56:46 -0500

    fucional ok

--- function_app.py
+++ function_app.py
@@ -8,62 +8,44 @@ AVRO_SCHEMA = {
     "name": "ProductosAvro",
     "namespace": "org.productos.avro",
     "fields": [
-        {
-            "name": "Id",
-            "type": "string"
-        },
-        {
-            "name": "nombre",
-            "type": "string"
-        },
-        {
-            "name": "fecha",
-            "type": "string"
-        },
-        {
-            "name": "unidades",
-            "type": "int"
-        },
+        {"name": "id", "type": "string"},
+        {"name": "nombre", "type": "string"},
+        {"name": "fecha", "type": "string"},
+        {"name": "unidades", "type": "int"},
         {
             "name": "categoria",
             "type": {
                 "type": "enum",
                 "name": "CategoriaEnumAvro",
-                "symbols": [
-                    "GRANOS",
-                    "LACTEOS",
-                    "FRUTAS"
-                ]
-            }
+                "symbols": ["GRANOS", "LACTEOS", "FRUTAS"],
+            },
         },
-        {
-            "name": "disponible",
-            "type": "boolean"
-        }
-    ]
+        {"name": "disponible", "type": "boolean"},
+    ],
 }
 
 # Definir la función de Azure
 app = func.FunctionApp()
 
+
 @app.function_name(name="procesar_avro")
 @app.route(route="procesar_avro")
 def procesar_avro(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Received request to process Avro data.')
+    logging.info("Received request to process Avro data.")
 
     try:
         # Obtener el cuerpo del mensaje
         avro_data = req.get_body()
-        logging.info(f'Received Avro data: {avro_data}')
+        logging.info(f"Received Avro data: {avro_data}")
 
         # Leer el mensaje Avro
         bytes_reader = io.BytesIO(avro_data)
         reader = fastavro.reader(bytes_reader, reader_schema=AVRO_SCHEMA)
-        logging.info('Avro data read successfully.')
+        logging.info("Avro data read successfully.")
 
         # Procesar cada registro
         for record in reader:
-            logging.info(f'Received record: {record}')
+            logging.info(f"Received record: {record}")
 
         return func.HttpResponse("Processed Avro data successfully.", status_code=200)
 
